515e7756c3eed75a8d6443f996beeaef12f87d01
James Wanyoike Mburu <jmburu280@gmail.com>
2022-06-11 14:49:42 +0300

    commit tree e3f076e69b281e4b5961d9359d22ea9986dd04e6

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+let messages = []
+function writeCards(names, event){
+    for (let counter = 0; counter < names.length;) {
+        let message = `Thank you, ${names[counter]}, for the wonderful ${event} gift!`;
+        messages.push(message)
+        counter = counter + 1
+      }
+    return messages
+}
+
+function countDown(input_number){
+    while (input_number >= 0) {
+        console.log(input_number)
+        input_number--
+      }
+}
